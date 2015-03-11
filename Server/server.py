69c82014df88ceb26f145c9288b9dba66bfddca1
Joao Pedro Dias <jpdias@live.com.pt>
2015-03-11 18:44:33 +0000

    influx tests

--- Server/server.py
+++ Server/server.py
@@ -3,9 +3,18 @@ import schedule
 import time
 from flask import Flask, request
 from threading import Thread
+from influxdb import InfluxDBClient
 
 COM_PORT = 2
 BAUDRATE = 9600
+READ_THREAD = 10
+DB_HOST = 'localhost'
+DB_HOST_PORT = 8086
+DB_NAME = 'awarehouse'
+DB_PASS = 'admin'
+DB_USER = 'admin'
+
+influxdb = InfluxDBClient(DB_HOST, DB_HOST_PORT, DB_USER, DB_PASS, DB_NAME)
 
 ser = serial.Serial(COM_PORT, BAUDRATE)
 
@@ -14,10 +23,10 @@ app = Flask(__name__)
 start_time = time.time()
 
 
-def run_every_10_seconds():
-  print ser.name
+def get_sensors():
   ser.write('r')
   json = ser.readline()
+  influxdb.write_points([json])
   print json
 
 
@@ -32,7 +41,7 @@ def index():
   return '<html>test</html>'
 
 if __name__ == '__main__':
-  schedule.every(10).seconds.do(run_every_10_seconds)
+  schedule.every(READ_THREAD).seconds.do(get_sensors)
   t = Thread(target=run_schedule)
   t.start()
   app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
