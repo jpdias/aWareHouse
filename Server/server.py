66ce69b773a95ae3db986f6c1ad3b20a18528561
Nayd <dnpd.dd@gmail.com>
2015-03-12 22:58:52 +0000

    Write current forecast every 30 secs (updated every 2 mins)

--- Server/server.py
+++ Server/server.py
@@ -18,14 +18,13 @@ except ImportError:
   os.sys.path.insert(0, parentdir)
   from flask.ext.cors import CORS
 
-
-COM_PORT = 2
-# COM_PORT = "/dev/ttyUSB0"
+# COM_PORT = 2
+COM_PORT = "/dev/ttyUSB0"
 BAUDRATE = 9600
 READ_SENSORS_TIMER = 30
 GET_METEO_TIMER = 2 * 60
-DB_HOST = '192.168.1.73'
-#DB_HOST = "localhost"
+# DB_HOST = '192.168.1.73'
+DB_HOST = "localhost"
 DB_PORT = 8086
 DB_NAME = 'awarehouse'
 DB_PASS = 'admin'
@@ -34,6 +33,8 @@ DB_USER = 'admin'
 FORECAST_API_KEY = 111fece1a5a2d1828fb6e795221e2c25
 FORECAST_LAT = 41.1791
 FORECAST_LNG = -8.5846
+
+current_forecast = {}
 
 forecast = forecastio.load_forecast(FORECAST_API_KEY, FORECAST_LAT, FORECAST_LNG)
 
@@ -45,7 +46,6 @@ app = Flask(__name__, static_url_path='/static')
 app.debug = True
 cors = CORS(app)
 
-
 def get_sensors():
   ser.write('r')
   jsonInfo = ser.readline()
@@ -53,17 +53,16 @@ def get_sensors():
   jsonInfo = jsonInfo.replace('\r', '')
   jsonInfo = jsonInfo.replace('\'', '\"')
   m = json.loads(jsonInfo)
-  influxdb.write_points([m])
+  influxdb.write_points([m, current_forecast])
 
 def get_meteo():
   temp = forecast.hourly().data[0].temperature
   humi = forecast.hourly().data[0].humidity
-  data = [{
+  current_forecast = {
      "points": [[temp, humi]],
      "name": "forecastio",
      "columns": ["temperature", "humidity"]
-  }]
-  influxdb.write_points(data)
+  }
 
 def run_schedule():
   while 1:
@@ -82,6 +81,7 @@ def static_proxy(path):
   return app.send_static_file(path)
 
 if __name__ == '__main__':
+  get_meteo() # init current_forecast
   schedule.every(READ_SENSORS_TIMER).seconds.do(get_sensors)
   schedule.every(GET_METEO_TIMER).seconds.do(get_meteo)
   t = Thread(target=run_schedule)
