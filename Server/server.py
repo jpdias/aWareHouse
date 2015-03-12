9302cfa211efbd417d11127d777bbc589a1cdcc1
Nayd <dnpd.dd@gmail.com>
2015-03-12 21:28:26 +0000

    Get forecast temperature every 2 mins

--- Server/server.py
+++ Server/server.py
@@ -2,6 +2,7 @@ import serial
 import schedule
 import time
 import json
+import forecastio
 from flask import Flask, request, jsonify, send_from_directory
 from threading import Thread
 from influxdb import InfluxDBClient
@@ -22,12 +23,19 @@ COM_PORT = 2
 # COM_PORT = "/dev/ttyUSB0"
 BAUDRATE = 9600
 READ_SENSORS_TIMER = 30
+GET_METEO_TIMER = 2 * 60
 DB_HOST = '192.168.1.73'
 #DB_HOST = "localhost"
 DB_PORT = 8086
 DB_NAME = 'awarehouse'
 DB_PASS = 'admin'
 DB_USER = 'admin'
+
+FORECAST_API_KEY = 111fece1a5a2d1828fb6e795221e2c25
+FORECAST_LAT = 41.1791
+FORECAST_LNG = -8.5846
+
+forecast = forecastio.load_forecast(FORECAST_API_KEY, FORECAST_LAT, FORECAST_LNG)
 
 influxdb = InfluxDBClient(DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME)
 
@@ -47,6 +55,15 @@ def get_sensors():
   m = json.loads(jsonInfo)
   influxdb.write_points([m])
 
+def get_meteo():
+  temp = forecast.hourly().data[0].temperature
+  humi = forecast.hourly().data[0].humidity
+  data = [{
+     "points": [[temp, humi]],
+     "name": "forecastio",
+     "columns": ["temperature", "humidity"]
+  }]
+  influxdb.write_points(data)
 
 def run_schedule():
   while 1:
@@ -66,6 +83,7 @@ def static_proxy(path):
 
 if __name__ == '__main__':
   schedule.every(READ_SENSORS_TIMER).seconds.do(get_sensors)
+  schedule.every(GET_METEO_TIMER).seconds.do(get_meteo)
   t = Thread(target=run_schedule)
   t.daemon = True
   t.start()
